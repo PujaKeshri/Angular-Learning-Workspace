f2bff01bba0b300a920427c25ca998b97cbf0c6f
PujaKeshri <puja.keshri@intellectdesign.com>
2018-10-18 13:15:30 +0530

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {EnquiryComponent} from './enquiry/enquiry.component';
 import {PersonalDetails} from './services/personal-details.service';
@@ -20,6 +21,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,17 +2,46 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 @Component({
     selector:'login',
-    templateUrl:'./login.component.html'
+    templateUrl:'./login.component.html',
+    styleUrls:['./login.component.css']
 })
 export class LoginComponent implements OnInit{
+    @ViewChild('profilepicform') profilepicForm : NgForm;
     @ViewChild('loginform') loginForm : NgForm;
-
+    defaultImg : string = "../assets/images/male.png";
+    imageSrc : string = this.defaultImg;
     ngOnInit(){
         
     }
 
+    onFileChanged(event){
+      //  console.log(event);
+      //  console.log(this.profilepicForm.value);
+        const file :File = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = this.handleReaderLoad.bind(this);
+        reader.onerror = this.handleReaderError.bind(this);
+        reader.readAsDataURL(file);
+       // console.log("Url : "+URL.createObjectURL(file));
+        //  console.log("File : "+URL.revokeObjectURL(URL.createObjectURL(file)));
+    }
+
+    handleReaderLoad(e){
+        console.log("In side handleReaderLoad");
+        console.log(e.target.result);
+        console.log(btoa(e.target.result));
+        console.log(atob(btoa(e.target.result)));
+        this.imageSrc = e.target.result;
+    }
+
+    handleReaderError(e){
+        console.log("In side handleReaderError");
+        this.imageSrc = this.defaultImg;
+    }
+
     onSubmitClick(){
         console.log(this.loginForm);
+        console.log(this.loginForm.value);
     }
 
 }
